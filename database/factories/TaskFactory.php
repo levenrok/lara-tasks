fac16048a01c8028d4eb56baa36096505621debc
levenrok <levenrok@proton.me>
2025-09-04 23:09:21 +0530

    feat (model): create model for a task

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->sentence(),
+            'description' => fake()->paragraph(),
+            'date' => fake()->date(),
+            'completed' => false,
+        ];
+    }
+
+    public function completed(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'completed' => true,
+        ]);
+    }
+}
