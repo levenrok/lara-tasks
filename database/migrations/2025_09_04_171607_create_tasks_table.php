39a2dfe327e94fc0028c6c78676da6262470c307
levenrok <levenrok@proton.me>
2025-09-06 22:09:14 +0530

    fix (model): add relationship constraints to 'User' and 'Task' models

--- database/migrations/2025_09_04_171607_create_tasks_table.php
+++ database/migrations/2025_09_04_171607_create_tasks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
             $table->string('name');
             $table->text('description');
             $table->date('date');
