a54b2e9a6989c4381b6a28efbc0870678034361c
levenrok <levenrok@proton.me>
2025-09-06 22:11:57 +0530

    feat (controller): define the route to view all the tasks

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -10,6 +11,14 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('tasks', function () {
+    $user_tasks = auth()->user()->tasks()->get();
+
+    return Inertia::render('Tasks', [
+        'user_tasks' => $user_tasks,
+    ]);
+})->middleware(['auth', 'verified'])->name('tasks');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
