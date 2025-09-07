043e52891085c088b12eb25252e74e5731807943
levenrok <levenrok@proton.me>
2025-09-07 16:10:49 +0530

    feat (controller): define a wildcard route to display info about a single task

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -8,17 +7,25 @@ Route::get('/', function () {
     return Inertia::render('Home');
 })->name('home');
 
-Route::get('dashboard', function () {
+Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('tasks', function () {
+Route::get('/tasks', function () {
     $user_tasks = auth()->user()->tasks()->get();
 
     return Inertia::render('Tasks', [
         'user_tasks' => $user_tasks,
     ]);
 })->middleware(['auth', 'verified'])->name('tasks');
+
+Route::get('/tasks/{id}', function ($id) {
+    $user_task = auth()->user()->tasks()->find($id);
+
+    return Inertia::render('Task', [
+        'user_task' => $user_task,
+    ]);
+})->middleware(['auth', 'verified'])->name('task');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
