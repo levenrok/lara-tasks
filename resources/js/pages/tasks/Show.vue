f17859bca253740831933d938420d63da21cf0ec
levenrok <levenrok@proton.me>
2025-09-11 22:05:17 +0530

    feat (view): create the page to edit a task

--- resources/js/pages/tasks/Show.vue
+++ resources/js/pages/tasks/Show.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
 import { Task } from '@/types/task';
-import { Head, useForm } from '@inertiajs/vue3';
-
-const delete_form = useForm({});
+import { Head, Link, useForm } from '@inertiajs/vue3';
 
 defineProps<{
     task: Task;
 }>();
+
+const delete_form = useForm({});
 </script>
 
 <template>
@@ -32,12 +32,20 @@ defineProps<{
                 <p :class="{ 'text-bold text-green-500 underline': task.completed }">Complete</p>
                 <p :class="{ 'text-bold text-red-500 underline': !task.completed }">Not Complete</p>
             </div>
-            <button
-                class="rounded-md bg-red-600 px-3 py-2 text-sm font-bold text-white shadow-xs hover:bg-red-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
-                form="delete"
-            >
-                Delete
-            </button>
+            <div class="flex justify-between">
+                <Link
+                    :href="`/tasks/${task.id}/edit`"
+                    class="rounded-md bg-orange-600 px-3 py-2 text-sm font-bold text-white shadow-xs hover:bg-orange-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
+                >
+                    Edit
+                </Link>
+                <button
+                    class="rounded-md bg-red-600 px-3 py-2 text-sm font-bold text-white shadow-xs hover:bg-red-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+                    form="delete"
+                >
+                    Delete
+                </button>
+            </div>
         </div>
         <form @submit.prevent="delete_form.delete(`/tasks/${task.id}`)" class="hidden" id="delete"></form>
     </AppLayout>
