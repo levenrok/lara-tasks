28fc2e2a9a173748d61072d4c5eecf428ad4a2f0
Leven Rochana <levenrok@proton.me>
2025-09-17 13:25:44 +0000

    test (view): create browsers tests for the home page

--- tests/Browser/HomeTest.php
+++ tests/Browser/HomeTest.php
@@ -1,7 +1,23 @@
 <?php
 
-test('page is rendered', function() {
+test('home page is displayed', function () {
     $page = visit(route('home'));
 
-    $page->assertSee('Welcome to LaraTasks');
+    $page->assertSee('Welcome to LaraTasks')
+        ->assertSeeLink('Log in')
+        ->assertSeeLink('Register');
+});
+
+test('login button takes to the login page', function () {
+    $page = visit(route('home'));
+
+    $page->click('Log in')
+        ->assertPathIs('/login');
+});
+
+test('register button takes to the registration page', function () {
+    $page = visit(route('home'));
+
+    $page->click('Register')
+        ->assertPathIs('/register');
 });
