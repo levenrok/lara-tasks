e5460f9d380e22aad0cc1954206434a196d5cc69
Leven Rochana <levenrok@proton.me>
2025-09-18 03:38:58 +0000

    feat (view): add a title for the table view

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -14,38 +14,43 @@ defineProps<{
     <Head title="Dashboard" />
 
     <AppLayout>
-        <Table>
-            <TableCaption>A list of your tasks.</TableCaption>
-            <TableHeader>
-                <TableRow>
-                    <TableHead class="w-[100px]"> Name </TableHead>
-                    <TableHead class="hidden sm:table-cell">Description</TableHead>
-                    <TableHead class="hidden sm:table-cell">Date</TableHead>
-                    <TableHead class="text-center"> Completed </TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                <TableRow v-for="task in tasks" :key="task.id">
-                    <TableCell class="font-medium">
-                        {{ task.name }}
-                    </TableCell>
-                    <TableCell class="hidden max-w-[400px] whitespace-normal sm:table-cell">
-                        {{ task.description }}
-                    </TableCell>
-                    <TableCell class="hidden sm:table-cell">
-                        {{ task.date }}
-                    </TableCell>
-                    <TableCell class="text-center">
-                        <input
-                            type="checkbox"
-                            :id="`task-${task.id}`"
-                            :checked="task.completed"
-                            @change="updateCompleted(task, ($event.target as HTMLInputElement).checked)"
-                            @click.stop
-                        />
-                    </TableCell>
-                </TableRow>
-            </TableBody>
-        </Table>
+        <div class="m-4 flex flex-col space-y-2">
+            <div class="rounded-lg border border-gray-200 p-2">
+                <h2 class="border-b border-gray-400 p-2 text-xl">Tasks</h2>
+                <Table>
+                    <TableCaption>A list of your tasks.</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead class="max-w-[100px]"> Name </TableHead>
+                            <TableHead class="hidden sm:table-cell">Description</TableHead>
+                            <TableHead class="hidden sm:table-cell">Date</TableHead>
+                            <TableHead class="text-center"> Completed </TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow v-for="task in tasks" :key="task.id">
+                            <TableCell class="font-medium whitespace-normal">
+                                {{ task.name }}
+                            </TableCell>
+                            <TableCell class="hidden max-w-[400px] whitespace-normal sm:table-cell">
+                                {{ task.description }}
+                            </TableCell>
+                            <TableCell class="hidden sm:table-cell">
+                                {{ task.date }}
+                            </TableCell>
+                            <TableCell class="text-center">
+                                <input
+                                    type="checkbox"
+                                    :id="`task-${task.id}`"
+                                    :checked="task.completed"
+                                    @change="updateCompleted(task, ($event.target as HTMLInputElement).checked)"
+                                    @click.stop
+                                />
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </div>
+        </div>
     </AppLayout>
 </template>
